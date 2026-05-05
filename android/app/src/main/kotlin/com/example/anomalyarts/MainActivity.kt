6a5a8b426bd96099a3dceff77a74439cce992490
jjoserdsgarcia <garciaedsonrv@gmail.com>
2026-05-05 19:35:36 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/anomalyarts/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.anomalyarts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/paginainicial.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Paginainicial extends StatefulWidget {
+  const Paginainicial({super.key});
+
+  @override
+  State<Paginainicial> createState() => _PaginainicialState();
+}
+
+class _PaginainicialState extends State<Paginainicial> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
