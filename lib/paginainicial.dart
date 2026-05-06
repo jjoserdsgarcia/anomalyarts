71b2d8c4de347ec911fd3ea7561e4e6ef1ade499
jjoserdsgarcia <garciaedsonrv@gmail.com>
2026-05-05 21:59:28 -0300

    .

--- lib/paginainicial.dart
+++ lib/paginainicial.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:appbar_animated/appbar_animated.dart';
 
 class Paginainicial extends StatefulWidget {
   const Paginainicial({super.key});
@@ -11,9 +10,6 @@ class Paginainicial extends StatefulWidget {
 class _PaginainicialState extends State<Paginainicial> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Anomaly Arts')),
-      body: const Center(child: Text('Bem-vindo à Página Inicial!')),
-    );
+    return Scaffold();
   }
 }
